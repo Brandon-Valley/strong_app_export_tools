cfb2cc2e35b8bdab596017df269e9bb1acdbbaaa
Brandon Valley <bavvh8@mst.edu>
2018-11-08 14:15:18 -0600

    cleaned up a bit

--- plot_tests.py
+++
@@ -1,8 +0,0 @@
-import plotly
-import plotly.graph_objs as go
-
-plotly.offline.plot({
-    "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1]), go.Scatter(x=[12, 22, 32, 24], y=[4, 32, 22, 1])],
-    "layout": go.Layout(title="hello world"),
-}, auto_open=True, filename = 'folder1/test_file_111.html')
-print('done!')
